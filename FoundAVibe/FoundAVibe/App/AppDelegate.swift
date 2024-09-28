bfcb14a9adce99cec6766c9cf3696b533f49d1bd
Alexander Cleoni <alexandercleoni@mac.lan>
2024-09-27 23:36:55 -0400

    add login screen

---
+++ FoundAVibe/FoundAVibe/App/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  FoundAVibe
+//
+//  Created by Alexander Cleoni on 9/21/24.
+//
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ FoundAVibe/FoundAVibe/Views/Components/TextFieldView.swift
@@ -0,0 +1,64 @@
+//
+//  TextFieldView.swift
+//  Found A Vibe
+//
+//  Created by Alexander Cleoni on 9/13/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct TextFieldView: View {
+    @Binding var input: String
+    @Binding var isSecure: Bool
+    
+    let keyboardType: UIKeyboardType
+    let iconName: String
+    let placeholder: String
+    
+    init(
+        input: Binding<String>,
+        isSecure: Binding<Bool>? = nil,
+        keyboardType: UIKeyboardType = .default,
+        iconName: String = "magnifyingglass",
+        placeholder: String = "Search"
+    ) {
+        _input = input
+        _isSecure = isSecure ?? .constant(false)
+        self.keyboardType = keyboardType
+        self.iconName = iconName
+        self.placeholder = placeholder
+    }
+    
+    @ViewBuilder
+    private var inputField: some View {
+        if isSecure {
+            SecureField(placeholder, text: $input)
+        } else {
+            TextField(placeholder, text: $input)
+        }
+    }
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(systemName: iconName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 24, height: 24)
+            inputField
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+        .background(Color.white)
+        .clipShape(.rect(cornerRadius: 8))
+    }
+}
+
+#Preview {
+    TextFieldView(input: .constant(""), keyboardType: .emailAddress, iconName: "envelope.fill", placeholder: "info@youremail.com")
+        .padding()
+}
